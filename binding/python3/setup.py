d57dbc5335370901849f5e6bcdadd2a7b22555d6
romain <martinez.staps@gmail.com>
2018-12-07 09:44:21 -0500

    add numpy in setup.py

--- binding/python3/setup.py
+++ binding/python3/setup.py
@@ -1,11 +1,6 @@
-import os
-import sys
-
 from setuptools import setup
 
-# Use versioneer to set version automatically
-
-
+# todo: use versioneer to set version automatically
 setup(name='ezc3d',
       version=0.3.3,
       description='Easy to use C3D reader/writer in C++, Python and Matlab',
@@ -14,7 +9,8 @@ setup(name='ezc3d',
       url='https://github.com/pyomeca/ezc3d',
       license='MIT',
       packages=['ezc3d'],
-      package_data={'ezc3d': ['_*.*', '*.dylib', '*.dll', '*.so*']},
+      install_requires=['numpy'],
+      #package_data={'ezc3d': ['_*.*', '*.dylib', '*.dll', '*.so*']},
       include_package_data=True,
       classifiers=[
           'Intended Audience :: Science/Research',
